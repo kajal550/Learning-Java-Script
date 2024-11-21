50b5ea6c117ff136ccbe53c8bd26d80a1dc7f042
kajal550 <kajalgoyal550@gmail.com>
2024-11-21 11:59:03 +0530

    Let Var Cons

---
+++ script.js
@@ -0,0 +1,42 @@
+// Var Let Const
+//VAR
+var age=10;
+
+if(true){
+    console.log(age);
+}
+
+function solve(){
+    var age=25;
+    console.log(age);
+}
+console.log(age);
+solve();
+
+//LET
+
+let a=10;
+console.log(a);
+
+{
+let a=10;
+console.log(a);
+}
+
+let x=10;
+if(true) {
+    let y=20;
+    console.log(x);
+    console.log(y);
+}
+
+let names=15;
+console.log(names);
+names="mayanai";
+console.log(names);
+
+//const
+
+const number=24;
+console.log(number);
+
