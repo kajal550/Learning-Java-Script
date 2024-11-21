4ce8de12a35cc3cf8137051c080a60d45553f77e
kajal550 <kajalgoyal550@gmail.com>
2024-11-21 12:20:12 +0530

    Primitive Operators

---
+++ Operators.js
@@ -0,0 +1,52 @@
+let marks=20;
+console.log(marks);
+
+//add,sub,multiply,div
+
+let p=10;
+let q=20;
+console.log(p+q);
+console.log(p-q);
+console.log(p*q);
+console.log(p/q);
+
+
+//boolean
+
+marks =true;
+console.log(typeof (marks));
+
+
+//undefined
+
+let names;
+console.log(names);
+
+//null
+
+let school=null;
+console.log(school);
+
+//Bigint
+
+let numbers=235545.454542
+console.log(numbers);
+
+//symbol
+
+let mySymbol=Symbol("description");
+console.log(mySymbol);
+
+let email=Symbol();
+let employee={};
+employee.name='john';
+employee.age='53';
+employee[email]='test@test.com';
+console.log(Object.keys(employee));
+
+for (const key in employee){
+    console.log(key);
+}
+
+console.log(employee[email]);
+console.log(employee)
