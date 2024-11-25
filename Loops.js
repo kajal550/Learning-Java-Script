1aa3b15f056cb11c3bb03405e0ba89d95388269e
kajal550 <kajalgoyal550@gmail.com>
2024-11-25 17:17:11 +0530

    LOOPS

---
+++ Loops.js
@@ -0,0 +1,20 @@
+// let p = 4;
+// while (p>5)
+// {
+// console.log(p);
+// p--;
+// }
+
+
+// let j = 0;
+// do
+// {
+//     console.log(j);
+// j--;
+// } while (j>0);
+
+
+for(let i = 3;i>0;i--)
+{
+    console.log(i);
+}
