af2b982a3dfa25d80655268b74f7a355d39acd06
kajal550 <kajalgoyal550@gmail.com>
2024-11-26 15:10:15 +0530

    Loosp Example

---
+++ LoopsdDouble.js
@@ -0,0 +1,27 @@
+// let marks = [2,4,6,8,10];
+// for (let mark of marks){
+//     let sum =0;
+//     sum += 2 * mark;
+//     console.log(sum);
+// }
+
+
+
+let k = [2,4,6,8,10];
+for (i=0;i<5;i++){
+    let doubleNumber = k[i] * 2
+    console.log(doubleNumber)
+}
+
+
+
+
+
+let num = [2,4,6,8,10];
+let i = 0;
+do
+{
+console.log( 2 *num[i]);
+i++;
+}
+while(i<5);
