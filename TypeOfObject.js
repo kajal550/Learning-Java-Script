ccae6acf0180d4b36bb05313abc1aa8cfe2de9b7
kajal550 <kajalgoyal550@gmail.com>
2024-11-29 13:57:36 +0530

    Type of objects

---
+++ TypeOfObject.js
@@ -0,0 +1,48 @@
+const details = {
+    names:"Kajal",
+    age: 25,
+    language: "JS",
+}
+
+// for (const prop in details){
+//    console.log(prop)
+// }
+
+
+// for (const prop in details){
+//     const value = details [prop]
+
+//  console.log(prop,value)
+// }
+
+
+// const keys= Object.keys(details)
+// console.log(keys)
+
+
+// const keys= Object.keys(details)
+// keys.forEach (key=> {
+//     const value = details [key]
+//     console.log(key,value)
+// })
+
+
+// const values = Object.values(details)
+// console.log(values)
+
+
+// const values = Object.values(details)
+// values.forEach (value=> {
+//     console.log(value)
+// })
+
+
+// const entries = Object.entries(details)
+// console.log(entries)
+
+// const entries = Object.entries(details)
+// entries.forEach (entry=> {
+//     const key = entry [0]
+//     const value = entry [1]
+//     console.log(key,value)
+// })
